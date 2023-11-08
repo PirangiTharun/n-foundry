f41e22bae40fcde3e3885afeef571e53c2dd2185
Tharun Pirangi <pirangitharun2000@gmail.com>
2023-11-08 17:11:00 +0530

    particles out on scroll

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,15 @@
 import React, {useEffect, useState} from 'react'
 import Navbar from './Navbar'
 import './Home.css';
-import { Canvas, useThree} from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import {Model} from '../models/Capsule.jsx';
 import { Suspense } from "react";
 import {Model as CapsulePill} from '../models/CapsulePills.jsx';
+import {Model as Particles} from '../models/Particles.jsx';
 
 const Home = () => {
-  const [x, setX] = useState(400);
-  const [y, setY] = useState(400);
-  const [z, setZ] = useState(400);
   const [distance, setDistance] = useState(0);
-  // console.log(z, "z in home");
-
-  /**
-   * props.distance = Math.min(props.distance, 300);
-  props.distance = Math.max(props.dispose,0);
-   */
   return (
     <div>
         <Navbar />
@@ -26,7 +18,7 @@ const Home = () => {
             <p className='mainIntro'>AI, AR & Robotics</p>
 
             <button className='eqBtn'>Enquire now</button>
-            <div style={{height: '40vh', width: 'inherit'}}>
+            <div className='canvasContainer'>
               <Canvas onWheel={(e)=> setDistance(Math.min(Math.max(distance+e.deltaY/5, 0),300))} camera={{ position: [400, 400, 400], fov: 35}}>
                 {/* <color attach="background" args={[0xe2f4df]} /> */}
                 <ambientLight />
@@ -34,6 +26,7 @@ const Home = () => {
                 <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
                 <directionalLight intensity={0.8} position={[-6, 2, 2]} />
                 <Suspense fallback={null}>
+                    <Particles scale={35} distance={distance}/>
                     <CapsulePill scale={0.8} distance={distance} />
                     {/* <Model z={z} scale={0.3} setZ={setZ}/> */}
                 </Suspense>
